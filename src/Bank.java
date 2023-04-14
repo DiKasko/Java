3d60b789f8a1e1144ca06f96579526572778a41d
kasko <gumapukk@gmail.com>
2023-04-14 22:46:06 +0500

    Домашняя работа

---
+++ src/Bank.java
@@ -0,0 +1,26 @@
+public class Bank {
+    public static int Days(int distance) {
+        if (distance <= 20) {
+            return 1;
+        } else if (distance <= 40) {
+            return 2;
+        } else if (distance <= 60) {
+            return 3;
+        } else if (distance <= 80) {
+            return 4;
+        } else {
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+        int deliveryDistance = 94;
+        int deliveryDays = Days(deliveryDistance);
+
+        if (deliveryDays >= 0) {
+            System.out.println("Потребуется дней: " + deliveryDays);
+        } else {
+            System.out.println("К сожалению, доставка в указанную точку не осуществляется.");
+        }
+    }
+}
