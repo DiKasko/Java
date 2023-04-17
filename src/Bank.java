844df2a24ee17fdb479bbe67119c1622dd21c772
kasko <gumapukk@gmail.com>
2023-04-17 15:19:19 +0500

    Другой метод

--- src/Bank.java
+++ src/Bank.java
@@ -1,26 +1,14 @@
 public class Bank {
-    public static int Days(int distance) {
-        if (distance <= 20) {
-            return 1;
-        } else if (distance <= 40) {
-            return 2;
-        } else if (distance <= 60) {
-            return 3;
-        } else if (distance <= 80) {
-            return 4;
-        } else {
-            return -1;
-        }
-    }
+
 
     public static void main(String[] args) {
         int deliveryDistance = 95;
-        int deliveryDays = Days(deliveryDistance);
-
-        if (deliveryDays >= 0) {
+        if (deliveryDistance < 80) {
+            int deliveryDays = deliveryDistance / 20 + 1;
             System.out.println("Потребуется дней: " + deliveryDays);
         } else {
             System.out.println("К сожалению, доставка в указанную точку не осуществляется.");
         }
+
     }
 }
